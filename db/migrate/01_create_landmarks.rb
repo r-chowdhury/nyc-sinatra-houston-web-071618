65f84c95782c2aea9ab5d6efc01a786f5fd65876
Ryhan Chowdhury <ryhanchowdhury@apples-MacBook-Pro-2.local>
2018-08-09 11:26:02 -0500

    Done.

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,9 @@
-class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+class CreateLandmarks < ActiveRecord::Migration[4.2]
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :year_completed
+      # t.belongs_to :figure
+    end
+  end
 end
